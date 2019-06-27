279a4cc0216b7a6b0a683541c27522f822c13ba3
percy tataje <percytataje@lacafetalab.pe>
2019-06-27 16:58:15 -0500

    second_commit

--- server/server.js
+++ server/server.js
@@ -1,41 +1,22 @@
 require('./config/config');
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
+app.use( require('./routes/usuario'));
 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario');
-})
-
-app.post('/usuario', function (req, res) {
-  let body = req.body;
-
-  if(body.nombre === undefined) {
-    res.status(400).json({
-      ok: false,
-      message: 'El nombre es necesario'
-    });
-  } else {
-    res.json({
-      body
-    })
+mongoose.connect('mongodb://localhost:27017/cafe',{ useNewUrlParser: true, useCreateIndex: true}, (err, res) => {
+  if( err) {
+    throw err;
   }
-  
-})
-app.put('/usuario/:id', function (req, res) {
-  let id = req.params.id;
-  res.json({
-    id
-  })
-})
- 
-app.delete('/', function (req, res) {
-  res.json('delete usuario')
-})
+  console.log('Base de datos ONLINE');
+});
  
 app.listen(process.env.PORT, () => {
   console.log('Escuchando el puerto:', process.env.PORT);
